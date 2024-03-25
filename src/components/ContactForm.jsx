a93081b6e9797502fb5350dd282451f77cc8e11f
xaver420 <158455285+xaverEbets@users.noreply.github.com>
2024-03-25 11:48:43 +0100

    formUpdate

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,20 +1,37 @@
 import React from "react";
 
 const ContactForm = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const inputs = document.querySelectorAll('input');
+        const textarea = document.querySelector('textarea');
+        const data = {
+            name: inputs[0].value,
+            lastName: inputs[1].value,
+            email: inputs[2].value,
+            message: textarea.value
+        }
+        console.log(data);
+        inputs.forEach(input => input.value = '');
+        textarea.value = '';
+
+        alert('Nachricht gesendet');
+    }
   return (
     <div id={'contact'} className={"section contactForm"}>
       <h2 className={"sec3"}>Kontaktieren Sie uns</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
           <div className={'inputDiv'}>
               <div className={"leftInput"}>
-                  <input type="text" placeholder="Name"/>
-                  <input type="email" placeholder="Nachname"/>
-                  <input type="text" placeholder="Email"/>
+                  <input minLength={1} type="text" placeholder="Name"/>
+                  <input minLength={1} type="text" placeholder="Nachname"/>
+                  <input minLength={10}  type="email" placeholder="Email"/>
               </div>
 
-              <textarea placeholder="Nachricht"/>
+              <textarea minLength={5} placeholder="Nachricht"/>
           </div>
-          <button type="submit">Senden</button>
+          <button  type="submit">Senden</button>
       </form>
     </div>
   );
