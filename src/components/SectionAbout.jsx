9eed42896a5c3c8232117ebab298a7fdca245835
xaver420 <158455285+xaverEbets@users.noreply.github.com>
2024-03-25 14:18:27 +0100

    styleUpdates

--- src/components/SectionAbout.jsx
+++ src/components/SectionAbout.jsx
@@ -4,16 +4,16 @@ import img from "../resources/section3.png";
 const SectionAbout = () => {
   return (
     <>
-      <div id={'about'} className={'section sectionAbout'}>
+      <div id={"about"} className={"section sectionAbout"}>
         <div className={"halfDiv"}>
-            <img src={img} className={"image"}/>
+          <img src={img} className={"image"} />
         </div>
         <div className={"halfDiv"}>
-            <div className={"textDiv"}>
-                <h2 className={'sec3'}>Über uns</h2>
-                <p>
-                    Bei EX Solutions sind wir darauf spezialisiert, Unternehmen online zum Erfolg zu führen. Unsere maßgeschneiderten Lösungen in Website-Design und Online-Marketing sind darauf ausgerichtet, die digitale Präsenz unserer Kunden zu maximieren und ihr Wachstum zu fördern. Vertraue auf unsere Expertise und begib dich mit uns auf den Weg zu deinem digitalen Erfolg.
-                </p>
+          <div className={"textDiv"}>
+            <h2 className={"sec3"}>Über uns</h2>
+            <p>
+                Unser Team besteht aus einem engagierten Zusammenschluss erfahrener Webentwickler, kreativer Webdesigner und versierter Online-Marketing-Experten. Wir sind immer auf dem neuesten Stand der Technik und setzen auf innovative Lösungen, um unseren Kunden bestmöglich zu dienen.
+            </p>
             </div>
         </div>
       </div>
--- src/components/SectionDienstleistungen.jsx
+++ src/components/SectionDienstleistungen.jsx
@@ -1,33 +1,33 @@
 import React, { useEffect, useRef } from "react";
 import img from "../resources/section2.png"; // Import des Bildes
 
-
 const SectionDienstleistungen = () => {
-
   return (
-      <div  id={'dienstleistungen'} className={"section dienstleistungen"}>
-        <div className={"halfDiv flyInLeft "}>
-          <div className={"textDiv"}>
-            <h2 id={'sec2'}>Dienstleistungen</h2>
+    <div id={"dienstleistungen"} className={"section dienstleistungen"}>
+      <div className={"halfDiv flyInLeft "}>
+        <div className={"textDiv"}>
+          <h2 id={"sec2"}>Dienstleistungen</h2>
+          <p>
+            Wir entwickeln moderne, responsive Websites, die auf Ihre
+            individuellen Bedürfnisse zugeschnitten sind. Von der Konzeption und
+            Gestaltung bis hin zur Entwicklung und Wartung bieten wir Ihnen
+            alles aus einer Hand.
+          </p>
+
             <p>
-              Wir bieten Ihnen eine Vielzahl an Dienstleistungen an. Unsere
-              Dienstleistungen sind auf Ihre Bedürfnisse zugeschnitten. Wir
-              unterstützen Sie bei der Umsetzung Ihrer Projekte und helfen Ihnen
-              dabei, Ihre Ziele zu erreichen.
+                Gerne beraten wir Sie unverbindlich zu unseren Dienstleistungen.
+
+
             </p>
-            <p>
-              Unsere Dienstleistungen umfassen unter anderem die Bereiche
-              Softwareentwicklung, Webentwicklung, App-Entwicklung, Beratung und
-              Schulung.
-            </p>
-          </div>
-        </div>
-        <div className={"halfDiv flyInRight"}>
-          <div className={"imgDiv"}>
-            <img src={img} alt="section2"/> {/* Verwenden Sie die importierte img-Variable */}
-          </div>
         </div>
       </div>
+      <div className={"halfDiv flyInRight"}>
+        <div className={"imgDiv"}>
+          <img src={img} alt="section2" />{" "}
+          {/* Verwenden Sie die importierte img-Variable */}
+        </div>
+      </div>
+    </div>
   );
 };
 
