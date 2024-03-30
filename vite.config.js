0239561eab697f299b980103e890ee96d23b64d9
xaver420 <158455285+xaverEbets@users.noreply.github.com>
2024-03-30 20:41:47 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/websiteXaver/',
-  build: {
-    rollupOptions: {
-      external: ['@mui/icons-material/Menu']
-    }
-  }
+  base: '/websiteXaver/'
 })
