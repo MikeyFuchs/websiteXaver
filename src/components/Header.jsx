fd99f57cc206f0e39a63e6fe9f1b45152442bca5
xaver420 <158455285+xaverEbets@users.noreply.github.com>
2024-03-30 20:48:52 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,43 @@
-import React from "react";
-import Logo from "../resources/exsolocutedbgremoved.png";
-import MenuIcon from "@mui/icons-material/Menu";
+import React from 'react'
+import Logo from '../resources/exsolocutedbgremoved.png'
+import { MenuIcon } from '@mui/icons-material/Menu'
 
 const Header = () => {
-    const toggleMenu = () => {
-        const mobileMenu = document.querySelector(".mobileMenu");
-        mobileMenu.classList.toggle("active");
-    };
+  const toggleMenu = () => {
+    const mobileMenu = document.querySelector('.mobileMenu')
+    mobileMenu.classList.toggle('active')
+  }
 
-    return (
-        <>
-            <div className={"Header"}>
-                <img src={Logo} alt="Logo" className={"logo"} />
-                <div className={"links"}>
-                    <a href={"#dienstleistungen"}>Dienstleistungen</a>
-                    <a href={"#projekte"}>Projekte</a>
-                    <a href={"#about"}>Über uns</a>
-                    <a href={"#contact"}>Kontakt</a>
-                </div>
-                <div onClick={toggleMenu} className={"menuIcon"}>
-                    <MenuIcon />
-                </div>
-            </div>
-            <div className={"mobileMenu"}>
-                <a onClick={toggleMenu} href={"#dienstleistungen"}>
-                    Dienstleistungen
-                </a>
-                <a onClick={toggleMenu} href={"#projekte"}>
-                    Projekte
-                </a>
-                <a onClick={toggleMenu} href={"#about"}>
-                    Über uns
-                </a>
-                <a onClick={toggleMenu} href={"#contact"}>
-                    Kontakt
-                </a>
-            </div>
-        </>
-    );
-};
+  return (
+    <>
+      <div className={'Header'}>
+        <img src={Logo} alt='Logo' className={'logo'} />
+        <div className={'links'}>
+          <a href={'#dienstleistungen'}>Dienstleistungen</a>
+          <a href={'#projekte'}>Projekte</a>
+          <a href={'#about'}>Über uns</a>
+          <a href={'#contact'}>Kontakt</a>
+        </div>
+        <div onClick={toggleMenu} className={'menuIcon'}>
+          <MenuIcon />
+        </div>
+      </div>
+      <div className={'mobileMenu'}>
+        <a onClick={toggleMenu} href={'#dienstleistungen'}>
+          Dienstleistungen
+        </a>
+        <a onClick={toggleMenu} href={'#projekte'}>
+          Projekte
+        </a>
+        <a onClick={toggleMenu} href={'#about'}>
+          Über uns
+        </a>
+        <a onClick={toggleMenu} href={'#contact'}>
+          Kontakt
+        </a>
+      </div>
+    </>
+  )
+}
 
-
-export default Header;
+export default Header
